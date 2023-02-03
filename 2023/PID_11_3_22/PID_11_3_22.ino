8a77e4f7c72c2bacc0bc062277d25ac85eb4c103
davidhefter <81720248+davidhefter@users.noreply.github.com>
2023-02-02 19:15:03 -0800

    Update PID_11_3_22.ino

--- 2023/PID_11_3_22/PID_11_3_22.ino
+++ 2023/PID_11_3_22/PID_11_3_22.ino
@@ -1,77 +1,87 @@
-#define ChA 2
+#define ENA 7
 #define PWM 9
-#define INA 7
+#define ChA 2
 
+#define DPR 360.0 // Degrees rotated by joint per revolution of motor: 360 / (Revolutions of Motor / 1 Revolution of Joint)
+#define TPR 11.0 // Hall Effect Sensor Ticks per Revolution of motor
 
+#define minSPD 5
+#define maxSPD 20
 
-#define ERROR_MARGIN 5 
-#define DT 0.000000001
-#define Kp 1.5
+#define ERROR_MARGIN 0
+#define Kp 1
 #define Ki 0
 #define Kd 0
-#define gearRatio 1 // 1 tick = 360 degrees, motor geared down by gearRatio
 
 int tickCount = 0;
 
-unsigned long newTime = 0;
-unsigned long oldTime = 0;
+float DT = 0;
+float DPT = DPR / TPR;
+
+unsigned long time = 0;
+unsigned long prevTime = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
  
-  pinMode (ChA, INPUT);
+  pinMode(ENA, OUTPUT);
   pinMode(PWM, OUTPUT);
-  pinMode(INA1, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(ChA), tickCounter, RISING);
-  oldTime = millis();
-  newTime = millis();
+  pinMode (ChA, INPUT);
+  attachInterrupt(digitalPinToInterrupt(ChA), tickCounter, RISING); //run tickCounter everytime the Hall Effect sensor triggers high
+  time = micros();
+  prevTime = micros();
 
-  digitalWrite(INA, HIGH);
-  turn(360);
-  
+  digitalWrite(ENA, LOW);
+  analogWrite(PWM, 0);
+  turn(3600);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
    
-   
 }
 
 void turn(int degs) {
-    tickCount = 0;
-    int sumError = 0;
-    int prevError = degs - tickCount;
-    while (abs(((360*tickCount)/gearRatio) - degs) > ERROR_MARGIN) {
-      int error = degs - ((360*tickCount)/gearRatio);
-      double P = Kp * error;
-      sumError += error * DT;
-      double I = Ki * sumError;
-      int changeError = (error - prevError) / DT;
-      double D = Kd * changeError;
-      int vel = (int)(P + I + D);
+   tickCount = 0;
+   int sumError = 0;
+   int prevError = degs - (DPT * tickCount); // Set previous error
+   while (abs((DPT * tickCount) - degs) > ERROR_MARGIN) {  // while the actual degrees minus the target degrees is greater than the error margin
+     int error = degs - (DPT * tickCount); // Set current error to the difference between target degrees and actual degrees
+     double P = Kp * error;
+     sumError += error * DT; // Integral of error with respect to time
+     double I = Ki * sumError;
+     int changeError = (error - prevError) / DT; // Derivative of error with respect to time
+     double D = Kd * changeError;
+     int vel = P + I + D;
      
-      analogWrite(PWM, min(vel, 255));
-      prevError = error;
-    // delay(DT * 1000);
-    // Serial.print(tickCount);
-    // Serial.print(" ");
-    // Serial.print(P);
-    // Serial.print(" ");
-    // Serial.print(I);
-    // Serial.print(" ");
-    // Serial.print(D);
-    // Serial.print(" ");
-    // Serial.println(vel);
+     digitalWrite(ENA, HIGH);
+     analogWrite(PWM, min(abs(vel), maxSPD));
+     prevError = error;
+     /*Serial.print(" ");
+     Serial.print(tickCount / TPR);
+     Serial.print(" ");
+     Serial.print(P);
+     Serial.print(" ");
+     Serial.print(I);
+     Serial.print(" ");
+     Serial.print(D);
+     Serial.print(" ");
+     Serial.println(vel);*/
    }
-   
-   analogWrite(PWM,0);
+  
+  analogWrite(PWM, 0);
+  delay(1000);
+  digitalWrite(ENA, LOW);
+  Serial.println(tickCount);
+  Serial.println(tickCount / TPR);
 }
 
  
 void tickCounter() {
   tickCount++;
-  oldTime = newTime
-  newTime = millis()
-  DT = newTime-oldTime
+  prevTime = time;
+  time = micros();
+  DT = time - prevTime;
 }
