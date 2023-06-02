8fe8202921df0b6477979a128e4d31b30d39de13
davidhefter <81720248+davidhefter@users.noreply.github.com>
2023-06-01 19:39:21 -0700

    Update Lleap_velocity_test.ino
    Added enable/direction pins, changed velocity test from 0%-100% to 0%-40%

--- 2023/Lleap_velocity_test/Lleap_velocity_test.ino
+++ 2023/Lleap_velocity_test/Lleap_velocity_test.ino
@@ -1,10 +1,12 @@
-#define ChA 2
-#define PWM 9
+#define ChA 34
+#define DAC 25
 #define TPR 11
+#define EN 12
+#define DIR 14
 
 #define maxSpd 255 //analogWrite value corresponding to 5V which is ~2500 RPM
 
-unsigned long time = 0;
+unsigned long currentTime = 0;
 unsigned long prevTime = 0;
 
 void setup() {
@@ -12,7 +14,11 @@ void setup() {
   Serial.begin(115200);
   
   pinMode(ChA, INPUT);
-  pinMode(PWM, OUTPUT);
+  pinMode(DAC, OUTPUT);
+  pinMode(EN, INPUT);
+  pinMode(DIR, INPUT);
+  digitalWrite(EN, HIGH);
+  digitalWrite(DIR, HIGH);
   attachInterrupt(digitalPinToInterrupt(ChA), tickCounter, RISING); //run tickCounter everytime the Hall Effect sensor triggers high
 }
 
@@ -20,44 +26,44 @@ void loop() {
   //put your main code here, to run repeatedly:
   
   delay(5000);
-  analogWrite(PWM, 0);  //run at 0 RPM
+  dacWrite(DAC, 0);  //run at 0 RPM
   Serial.println(0);
   delay(5000);
-  analogWrite(PWM, (maxSpd * 10) / 100); //run at 10% max speed ~250 RPM
+  dacWrite(DAC, (maxSpd * 4) / 100); //run at 10% max speed ~250 RPM
   Serial.println(10);
   delay(5000);
-  analogWrite(PWM, (maxSpd * 20) / 100); //run at 20% max speed ~500 RPM
+  dacWrite(DAC, (maxSpd * 8) / 100); //run at 20% max speed ~500 RPM
   Serial.println(20);
   delay(5000);
-  analogWrite(PWM, (maxSpd * 30) / 100); //run at 20% max speed ~750 RPM
+  dacWrite(DAC, (maxSpd * 12) / 100); //run at 20% max speed ~750 RPM
   Serial.println(30);
   delay(5000);
-  analogWrite(PWM, (maxSpd * 40) / 100); //run at 40% max speed ~1000 RPM
+  dacWrite(DAC, (maxSpd * 16) / 100); //run at 40% max speed ~1000 RPM
   Serial.println(40);
   delay(5000);
-  analogWrite(PWM, (maxSpd * 50) / 100); //run at 50% max speed ~1250 RPM
+  dacWrite(DAC, (maxSpd * 20) / 100); //run at 50% max speed ~1250 RPM
   Serial.println(50);
   delay(5000);
-  analogWrite(PWM, (maxSpd * 60) / 100); //run at 60% max speed ~1500 RPM
+  dacWrite(DAC, (maxSpd * 24) / 100); //run at 60% max speed ~1500 RPM
   Serial.println(60);
   delay(5000);
-  analogWrite(PWM, (maxSpd * 70) / 100); //run at 70% max speed ~1750 RPM
+  dacWrite(DAC, (maxSpd * 28) / 100); //run at 70% max speed ~1750 RPM
   Serial.println(70);
   delay(5000);
-  analogWrite(PWM, (maxSpd * 80) / 100); //run at 80% max speed ~2000 RPM
+  dacWrite(DAC, (maxSpd * 32) / 100); //run at 80% max speed ~2000 RPM
   Serial.println(80);
   delay(5000);
-  analogWrite(PWM, (maxSpd * 90) / 100); //run at 90% max speed ~2250 RPM
+  dacWrite(DAC, (maxSpd * 36) / 100); //run at 90% max speed ~2250 RPM
   Serial.println(90);
   delay(5000);
-  analogWrite(PWM, min(((maxSpd * 100) / 100), 255)); //run at 100% max speed ~2500 RPM
+  dacWrite(DAC, min(((maxSpd * 40) / 100), 255)); //run at 100% max speed ~2500 RPM
   Serial.println(100);
   delay(5000);
-  analogWrite(PWM, 0);
+  dacWrite(DAC, 0);
 }
 
 void tickCounter() {
-  prevTime = time; // record new time as old time
-  time = micros(); //update new time
-  Serial.println((60000000 / (time - prevTime)) / TPR); // calculate RPM using difference between old and new time and ticks per revolution
+  prevTime = currentTime; // record new time as old time
+  currentTime = micros(); //update new time
+  Serial.println((60000000 / (currentTime - prevTime)) / TPR); // calculate RPM using difference between old and new time and ticks per revolution
 }
